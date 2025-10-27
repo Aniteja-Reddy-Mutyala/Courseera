f8ef9d14209532eee973781513249ad50f4356be
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 00:32:37 -0700

    Created ny first rust project hello world

---
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/debug/deps/hello_world-96f488716a7cfd8d.d
@@ -0,0 +1,5 @@
+/Users/anirudh/Desktop/Courseera/Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/debug/deps/hello_world-96f488716a7cfd8d.d: src/main.rs
+
+/Users/anirudh/Desktop/Courseera/Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/debug/deps/libhello_world-96f488716a7cfd8d.rmeta: src/main.rs
+
+src/main.rs:
