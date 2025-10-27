0ca817d04198f371eacfc7d9e32f567945ccb566
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 12:16:37 -0700

    cargo build --release co\mmand

---
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/release/deps/hello_world-25a5b9f8b5bbc9cb.d
@@ -0,0 +1,5 @@
+/Users/anirudh/Desktop/Courseera/Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/release/deps/hello_world-25a5b9f8b5bbc9cb.d: src/main.rs
+
+/Users/anirudh/Desktop/Courseera/Packt-Learn to code with Rust/Rust Basics and Core Concepts/hello_world/target/release/deps/hello_world-25a5b9f8b5bbc9cb: src/main.rs
+
+src/main.rs:
