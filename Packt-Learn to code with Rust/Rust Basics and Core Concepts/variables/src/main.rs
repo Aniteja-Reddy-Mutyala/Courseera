fe22b3fb2583595e192b6b849d1647c6fbf19298
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 14:38:22 -0700

    Created and use variables in rust.

---
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let apples = 50;
+    let oranges= 24+6;
+    let fruits=apples+oranges;
+    println!("My garden has {} apples and {} oranges",apples,oranges);
+    println!("My garden has {fruits} fruits."); 
+}
