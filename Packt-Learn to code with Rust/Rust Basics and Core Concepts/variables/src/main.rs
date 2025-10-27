4642ce70947495c513ca8c4991167ea2a1e2efd2
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 14:59:08 -0700

    Used mutable variables

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let apples = 50;
-    let oranges= 24+6;
-    let _fruits=apples+oranges;
-    println!("My garden has {0} apples and {1} oranges.I did i got my desired target of {0} apples",apples,oranges);
-    
+    let mut apples = 50;
+    let oranges = 24 + 6;
+    let _fruits = apples + oranges;
+    apples = 100;
+    println!(
+        "My garden has {0} apples and {1} oranges.I did i got my desired target of {0} apples",
+        apples, oranges
+    );
 }
