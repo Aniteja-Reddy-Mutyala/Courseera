20d77cc57bc5581506fa4b44a35f057f9733deb4
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 16:07:45 -0700

    solved the question

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_variables)]
 const YEAR:i32=1996;
+const TOUCHDOWN_POINTS:i32=6;
 type Number=i32;
 fn main() {
     let  apples :Number= 50;
@@ -16,4 +17,13 @@ fn main() {
         let  fruit_price=2;
         println!("Inside inner scope.No of fruits are {}.Total price is {}.",fruits,fruits*fruit_price);
     }
+    let season= "Fall";
+    let mut points_scored = 28;
+    points_scored = 35;
+    let event_time= "06:00";
+    let event_time= 6 ;
+    println!("My favourite season is {season}.The team scored {points_scored}.The time is {event_time}.A touchdown is worth {TOUCHDOWN_POINTS}.");
+
+    let favourite_beverage="Apple juice" ;
+     
 }
