f54cf010c4566c77f844f1cb3ca3c10826fc7940
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 15:16:57 -0700

    Explored scopes in rust

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    let mut apples = 50;
+    let  apples = 50;
     let oranges = "20";
     let oranges=20;
-    let _fruits = apples + oranges;
-    apples = 100;
+    let fruits = apples + oranges;
+    
     println!(
         "My garden has {0} apples and {1} oranges.I did i got my desired target of {0} apples",
         apples, oranges
     );
+    {    
+        let  fruit_price=2;
+        println!("Inside inner scope.No of fruits are {}.Total price is {}.",fruits,fruits*fruit_price);
+    }
 }
