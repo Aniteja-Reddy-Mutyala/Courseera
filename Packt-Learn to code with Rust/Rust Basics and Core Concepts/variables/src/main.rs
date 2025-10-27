9fceb40ae9a8a8a7db11003595d1aceb32274b63
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 14:53:20 -0700

    Used a underscore variable to ignore warnings like unused variables

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let apples = 50;
     let oranges= 24+6;
-    let fruits=apples+oranges;
+    let _fruits=apples+oranges;
     println!("My garden has {0} apples and {1} oranges.I did i got my desired target of {0} apples",apples,oranges);
-    println!("My garden has {fruits} fruits."); 
+    
 }
