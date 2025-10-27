c38c0cd77ea2a991eb8467dcf8dc45bda28cf4bc
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-27 14:44:18 -0700

    Using positional arguments

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/variables/src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     let apples = 50;
     let oranges= 24+6;
     let fruits=apples+oranges;
-    println!("My garden has {} apples and {} oranges",apples,oranges);
+    println!("My garden has {0} apples and {1} oranges.I did i got my desired target of {0} apples",apples,oranges);
     println!("My garden has {fruits} fruits."); 
 }
