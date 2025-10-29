d074bdf84cd00e073ea30e0115bdef716818484e
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-28 17:31:37 -0700

    Explored methods in rust

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/data_types/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/data_types/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     println!("I said \"Hi everyone\"");
     let path=r"HI.\n";
     println!("{path}");
+    let value:i32=-20;
+    println!("{}",value.abs());
+    let empty_space:&str="       messsage  ";
+    println!("{}",empty_space.trim());
+    println!("{}",value.pow(3));
 }
