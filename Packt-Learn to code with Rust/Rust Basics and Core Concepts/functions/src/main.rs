2efd50bce3a634653db6af883781163811b44c4e
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-29 00:06:26 -0700

    started with functions

---
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/functions/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    open_store();
+}
+fn open_store(){
+    println!("Welcome to my store");
+    about_store();
+}
+fn about_store(){
+    println!("This is my store");
+    thank_you();
+}
+fn thank_you(){
+    println!("Thank you for visiting my store");
+}
