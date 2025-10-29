0668562a0b028aba5d4b82e0209a39e7789a30ad
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-28 17:52:29 -0700

    Explored floating points and some of it's methods

--- Packt-Learn to code with Rust/Rust Basics and Core Concepts/data_types/src/main.rs
+++ Packt-Learn to code with Rust/Rust Basics and Core Concepts/data_types/src/main.rs
@@ -8,4 +8,9 @@ fn main() {
     let empty_space:&str="       messsage  ";
     println!("{}",empty_space.trim());
     println!("{}",value.pow(3));
+    let pi:f64=3.141598908765544321213445677888;
+    println!("The value of pi is : {pi}");
+    println!("Floor of the pi is {}",pi.floor());
+    println!("Ceil of pi is {}",pi.ceil());
+    println!("Pi is rounded to {}",pi.round());
 }
