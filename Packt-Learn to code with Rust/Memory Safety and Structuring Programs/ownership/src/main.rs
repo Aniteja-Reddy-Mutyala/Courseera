47e773cf5982ec1edbc4ec467e87936db65f9c67
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-30 00:16:07 -0700

    ownership in function parameters

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/ownership/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/ownership/src/main.rs
@@ -6,6 +6,17 @@ fn main() {
     let borrowed_value:&String=&my_string_value;
    
     println!("{borrowed_value:p}");
-   
+    let mut string="hi";
+    string="hello";
+   let  mut one:i32=5;
+   let two:&i32=&one;
+   let three:&i32=&one;
+   println!("{three}");
+  let apples:String=String::from("oranges");
+  print_my_value(apples);
+   println!("{apples}");
+}
 
+fn print_my_value(value:String){
+    println!("My value is {value}");
 }
