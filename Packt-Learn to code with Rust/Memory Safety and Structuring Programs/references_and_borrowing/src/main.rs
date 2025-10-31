ae59cfadd009dd890014fcb625aec77183b083f3
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-30 21:23:37 -0700

    Multiple immutable references

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/references_and_borrowing/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/references_and_borrowing/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     let mut current_meal:String=String::new();
     add_flour(& mut current_meal);
     show_my_meal(&current_meal);
+    let car:String=String::from("Red");
+    let car1:&String=&car;
+    let car2:&String=&car;
+    println!("{car1},{car2},{}",&car);
+
 }
 fn add_flour( meal:& mut String){
     meal.push_str("Add flour");
@@ -9,4 +14,5 @@ fn add_flour( meal:& mut String){
 }
 fn show_my_meal( meal:&String){
 print!("Meal steps:{meal}");
+
 }
