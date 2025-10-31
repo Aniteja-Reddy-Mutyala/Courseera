9fbe2d128213d0ae521cd34f64d45aa887708b87
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-31 00:57:07 -0700

    String literal slice vs String Slice

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/slices/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/slices/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-   let action_hero:String=String::from("Hi Batman");
+   /*let action_hero:String=String::from("Hi Batman");
    let first_part:&str=&action_hero[0..3];
    println!("{first_part}");
    let second_part:&str=&action_hero[3..9];
-   println!("{second_part}");
+   println!("{second_part}");*/
+   let first_part={
+    let action_hero="Batman";
+        &action_hero[0..5]
+   };
 
 }
