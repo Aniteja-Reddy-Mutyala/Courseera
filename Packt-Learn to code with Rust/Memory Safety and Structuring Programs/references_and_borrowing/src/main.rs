e62891f1934950eb210ac1ebaeea021215db3d15
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-30 19:43:48 -0700

    Immutable and Mutable Reference Parameters

---
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/references_and_borrowing/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut current_meal:String=String::new();
+    add_flour(& mut current_meal);
+    show_my_meal(&current_meal);
+}
+fn add_flour( meal:& mut String){
+    meal.push_str("Add flour");
+    
+}
+fn show_my_meal( meal:&String){
+print!("Meal steps:{meal}");
+}
