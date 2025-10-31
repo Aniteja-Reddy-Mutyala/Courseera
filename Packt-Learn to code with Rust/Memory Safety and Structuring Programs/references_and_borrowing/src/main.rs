0b9f8cbfe14848bfda219f9ea213fa681fcc296b
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-10-30 21:39:05 -0700

    mutable reference restrictions

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/references_and_borrowing/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/references_and_borrowing/src/main.rs
@@ -2,10 +2,12 @@ fn main() {
     let mut current_meal:String=String::new();
     add_flour(& mut current_meal);
     show_my_meal(&current_meal);
-    let car:String=String::from("Red");
-    let car1:&String=&car;
+    let mut car:String=String::from("Red");
+    let car1:& mut String=& mut car;
     let car2:&String=&car;
-    println!("{car1},{car2},{}",&car);
+    let car3:&String=&car;
+    println!("{car2},{}",&car);
+    // mutable and immutable reference to same address can't coexist at same time,i.e last used of mutable reference must be before the first use of immutable
 
 }
 fn add_flour( meal:& mut String){
