a866c52fbe0287aed3c8445d2135567c80afa127
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-01 11:19:57 -0700

    Some formatting in go

---
+++ Go programming for everyone /Part 1/formatting/formatting.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	var i int32 =55;
+	fmt.Println("Printing i with println function ",i);
+	fmt.Printf("printing with decimal format %d\n",i);
+	fmt.Printf("Printf with width 10d format i =%10d\n",i);
+	fmt.Printf("Printf with binary format i=%b\n",i);
+	fmt.Printf("Printf with o octal format i=%o\n",i);
+	fmt.Printf("printf with x hexadecimal format i=%x\n",i);
+	fmt.Printf("Printf with O octal format i=%O\n",i);
+
+
+
+}
