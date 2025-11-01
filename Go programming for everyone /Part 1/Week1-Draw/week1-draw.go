eefd60529013bad864e7ff5ef627a9478d4ff04b
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-01 11:35:14 -0700

    week-1 assignment

---
+++ Go programming for everyone /Part 1/Week1-Draw/week1-draw.go
@@ -0,0 +1,23 @@
+package main
+/*Drawing a sail boat */
+import "fmt"
+
+func main() {
+	fmt.Println("           |\\")
+	fmt.Println("           | \\")
+	fmt.Println("           |  \\")
+	fmt.Println("           |   \\")
+	fmt.Println("           |    \\")
+	fmt.Println("           |     \\")
+	fmt.Println("           |      \\")
+	fmt.Println("           |       \\")
+	fmt.Println("           |________\\")
+	fmt.Println("          /|         \\")
+	fmt.Println("         / |          \\")
+	fmt.Println("        /  |           \\")
+	fmt.Println("       /   |            \\")
+	fmt.Println("      /____|_____________\\")
+	fmt.Println("   ~~~|__________________|~~~")
+	fmt.Println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	fmt.Println(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+}
