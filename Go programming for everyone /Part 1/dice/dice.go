bd96fe2111b0fe2effc50e72bfce59980942d06d
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-02 11:18:41 -0800

    modified the previous to make it more general

--- Go programming for everyone /Part 1/dice/dice.go
+++ Go programming for everyone /Part 1/dice/dice.go
@@ -6,12 +6,14 @@ import (
 	"time"
 )
 func main(){
-	var pair,howMany int
+	var pair,howMany,value int
 	source :=rand.NewSource(time.Now().UnixNano())
 	rng:=rand.New(source)
+	fmt.Printf("Enter which value's probablilty you want in a dice");
+	fmt.Scanf("%d",&value)
 	for i:=0; i < 10000000; i++ {
 		pair=rng.Intn(6) +rng.Intn(6) + 2
-		if pair ==7{
+		if pair ==value{
 			howMany ++
 		}
 	}
