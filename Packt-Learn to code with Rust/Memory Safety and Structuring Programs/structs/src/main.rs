c257e2fffecf98468c1aa3f388a70d137147e305
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-02 18:03:03 -0800

    created struct instance inside a function

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
@@ -1,16 +1,21 @@
-fn main() {
-    struct Coffee {
+struct Coffee {
         price: f64,
         name: String,
         is_hot: bool,
     }
+fn main() {
+    
     let mut mocha: Coffee = Coffee {
         price: 4.99,
         name: String::from("Mocha"),
         is_hot: false,
     };
-
+    let latte=make_coffee(String::from("Latte"),3.99,true);
     println!("My morning {} costs {} and it is {} that it was hot.",mocha.name,mocha.price,mocha.is_hot);
     mocha.price=6.00;
     println!("The latest price of {} is {} ",mocha.name,mocha.price);
+    println!("The latest price of {} is {} and it is {} that it is hot.",latte.name,latte.price,latte.is_hot);
+}
+fn make_coffee(name:String,price:f64,is_hot:bool)->Coffee{
+        Coffee { price:price, name:name, is_hot:is_hot }
 }
