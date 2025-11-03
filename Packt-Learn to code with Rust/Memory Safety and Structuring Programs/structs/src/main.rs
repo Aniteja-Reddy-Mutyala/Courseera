8274330abfa4988e798bcd8849c7ea612ccf9b88
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-02 17:41:57 -0800

    Accessing struct fields

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
@@ -4,9 +4,10 @@ fn main() {
         name: String,
         is_hot: bool,
     }
-    let mocha: Coffee = Coffee {
+    let  mocha: Coffee = Coffee {
         price: 4.99,
         name: String::from("Mocha"),
         is_hot: false,
     };
+    println!("My morning {} costs {} and it is {} that it was hot.",mocha.name,mocha.price,mocha.is_hot);
 }
