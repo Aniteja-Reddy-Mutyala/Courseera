d8c79b93b2e082f945dfead7a1d967dbf808a8eb
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-02 17:47:06 -0800

    Modifying struct values

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/structs/src/main.rs
@@ -4,10 +4,13 @@ fn main() {
         name: String,
         is_hot: bool,
     }
-    let  mocha: Coffee = Coffee {
+    let mut mocha: Coffee = Coffee {
         price: 4.99,
         name: String::from("Mocha"),
         is_hot: false,
     };
+
     println!("My morning {} costs {} and it is {} that it was hot.",mocha.name,mocha.price,mocha.is_hot);
+    mocha.price=6.00;
+    println!("The latest price of {} is {} ",mocha.name,mocha.price);
 }
