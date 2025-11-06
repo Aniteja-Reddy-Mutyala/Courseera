eccbf91e0e74f5dee3147a909ea7be7dfa26411b
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 14:35:30 -0800

    Unwrap and expect method

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
@@ -7,5 +7,10 @@ fn main() {
    ];
    let bass=musical_instruments.get(2);
    println!("{bass:?}");
+   let valid_instrument=bass.unwrap();
+   println!("{valid_instrument}");
+   let invalid_instrument=musical_instruments.get(3);
+   //invalid_instrument.unwrap();
+   invalid_instrument.expect("Index out of bounds");
    
 }
