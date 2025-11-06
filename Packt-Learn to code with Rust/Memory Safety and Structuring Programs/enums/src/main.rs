5f01474d13ef311cbe98f7f9bb7ba3a59039dd5b
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 10:07:01 -0800

    match keyword-3

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -4,6 +4,22 @@ enum OperatingSystem{
     MacOS,
     Linux
 }
+#[derive(Debug)]
+enum LaundryCycle{
+    Cold,
+    Hot{
+        temperature:u32
+
+    },
+    Delicate(String)
+}
+fn wash_laundry(cycle:LaundryCycle){
+  match cycle{
+    LaundryCycle::Cold => {println!("Running the laundry in cold mode")},
+    LaundryCycle::Hot { temperature }=>{ println!(" Runnig the laundry with temperature of {temperature}");},
+    LaundryCycle::Delicate(fabric_type)=>{println!("Running the laundry with delicate cycle for {fabric_type}");}
+  }
+}
 fn main(){
     let my_os=OperatingSystem::MacOS;
     let age_os= years_since_release(my_os);
@@ -11,6 +27,10 @@ fn main(){
     let dad_os=OperatingSystem::Windows;
     let age_dad_os=years_since_release(dad_os);
     println!("My dad's computer is {age_dad_os} years.");
+    wash_laundry(LaundryCycle::Cold);
+    wash_laundry(LaundryCycle::Hot { temperature:55 });
+    wash_laundry(LaundryCycle::Delicate(String::from("Silk")));
+
 }
 fn years_since_release(os:OperatingSystem)->u32{
     match os {
@@ -21,5 +41,5 @@ fn years_since_release(os:OperatingSystem)->u32{
         OperatingSystem::MacOS =>23,
         OperatingSystem::Linux => 34
     }
-
+   
 }
