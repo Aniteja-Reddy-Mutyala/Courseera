f4f1c147b12949ce70b891c81a3875dcef3af993
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-05 22:59:33 -0800

    match keyword 2

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -8,10 +8,16 @@ fn main(){
     let my_os=OperatingSystem::MacOS;
     let age_os= years_since_release(my_os);
     println!("Age is {age_os}");
+    let dad_os=OperatingSystem::Windows;
+    let age_dad_os=years_since_release(dad_os);
+    println!("My dad's computer is {age_dad_os} years.");
 }
 fn years_since_release(os:OperatingSystem)->u32{
     match os {
-        OperatingSystem::Windows =>39 ,
+        OperatingSystem::Windows =>{
+           println!("Very old system");
+           39
+        } ,
         OperatingSystem::MacOS =>23,
         OperatingSystem::Linux => 34
     }
