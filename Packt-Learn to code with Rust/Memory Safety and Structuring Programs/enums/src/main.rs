8b79922e18cd26d25ffa95812e4efadcfae6e799
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 10:15:17 -0800

    methods in enum

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -13,13 +13,16 @@ enum LaundryCycle{
     },
     Delicate(String)
 }
-fn wash_laundry(cycle:LaundryCycle){
-  match cycle{
+impl LaundryCycle{
+fn wash_laundry(self:&Self){
+  match self{
     LaundryCycle::Cold => {println!("Running the laundry in cold mode")},
     LaundryCycle::Hot { temperature }=>{ println!(" Runnig the laundry with temperature of {temperature}");},
     LaundryCycle::Delicate(fabric_type)=>{println!("Running the laundry with delicate cycle for {fabric_type}");}
   }
 }
+}
+
 fn main(){
     let my_os=OperatingSystem::MacOS;
     let age_os= years_since_release(my_os);
@@ -27,9 +30,15 @@ fn main(){
     let dad_os=OperatingSystem::Windows;
     let age_dad_os=years_since_release(dad_os);
     println!("My dad's computer is {age_dad_os} years.");
-    wash_laundry(LaundryCycle::Cold);
-    wash_laundry(LaundryCycle::Hot { temperature:55 });
-    wash_laundry(LaundryCycle::Delicate(String::from("Silk")));
+    let wash_cold_clothes=LaundryCycle::Cold;
+    wash_cold_clothes.wash_laundry();
+    let wash_hot_mode=LaundryCycle::Hot { temperature:55 };
+    wash_hot_mode.wash_laundry();
+    let wash_delicate=LaundryCycle::Delicate(String::from("Silk"));
+    wash_delicate.wash_laundry();
+    // wash_laundry(LaundryCycle::Cold);
+    //wash_laundry(LaundryCycle::Hot { temperature:55 });
+    //wash_laundry(LaundryCycle::Delicate(String::from("Silk")));
 
 }
 fn years_since_release(os:OperatingSystem)->u32{
