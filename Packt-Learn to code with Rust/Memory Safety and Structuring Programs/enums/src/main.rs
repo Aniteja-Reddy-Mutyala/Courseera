ff2fcdd3bd97e1f50ec8100c8134b80123666288
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-05 22:28:35 -0800

    Nested enums

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -20,6 +20,17 @@ struct Card{
     rank:String,
     suit:CardSuit
 }
+#[derive(Debug)]
+enum Meat{
+    Chicken,
+    Steak
+}
+#[derive(Debug)]
+enum RestaurantItem{
+   Burrito(Meat),
+   Bowl(Meat),
+   VeganPlate
+}
 fn main() {
     let first_card:CardSuit=CardSuit::Hearts;
     let mut second_card:CardSuit=CardSuit::Spades;
@@ -37,6 +48,12 @@ fn main() {
     let mut my_payment_method= PaymentMethodType::CreditCard(String::from("123"));
     my_payment_method=paypal_credentials;
      println!("{my_payment_method:?}");
+     let lunch =RestaurantItem::Burrito(Meat::Steak);
+     let dinner =RestaurantItem::Bowl(Meat::Chicken);
+     let abandoned_meal= RestaurantItem::VeganPlate;
+     println!("Lunch was {lunch:?}");
+     println!("Dinner was {dinner:?}");
+     println!("Abandaoned meal was {abandoned_meal:?}");
      
     
 }
