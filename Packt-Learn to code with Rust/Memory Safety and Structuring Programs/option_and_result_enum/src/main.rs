1f44305664955e8c81537854164f91a49389be29
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 15:35:15 -0800

    Match with option enum

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
@@ -1,16 +1,22 @@
 fn main() {
+  
    let musical_instruments=[
     String::from("Guitar"),
     String::from("Drums"),
     String::from("bass"),
 
    ];
-   let bass=musical_instruments.get(2);
+   let bass=musical_instruments.get(3);
    println!("{bass:?}");
-   let valid_instrument=bass.unwrap();
-   println!("{valid_instrument}");
+   match bass {
+      
+      Option::Some(instrument)=>{println!("The instrument is {instrument}");}
+      Option::None=>{println!("Index out of bounds");}
+   }
+   //let valid_instrument=bass.unwrap();
+   //println!("{valid_instrument}");
    let invalid_instrument=musical_instruments.get(3);
    //invalid_instrument.unwrap();
-   invalid_instrument.expect("Index out of bounds");
+   //invalid_instrument.expect("Index out of bounds");
    
 }
