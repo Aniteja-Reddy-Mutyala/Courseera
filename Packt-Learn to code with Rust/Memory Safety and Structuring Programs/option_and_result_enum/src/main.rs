44d55dbde24ab03f075ef504cae161d8f3a2be05
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 14:14:20 -0800

    get method in array

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let a=Option::Some(5);
-    let b=Option::Some("hello");
-    let c=Option::Some(true);
-    let a:Option<i8>=Option::Some(4);
-    let a=Option::<i16>::Some(20);
-    let d=Option::<i32>::None;
+   let musical_instruments=[
+    String::from("Guitar"),
+    String::from("Drums"),
+    String::from("bass"),
+
+   ];
+   let bass=musical_instruments.get(2);
+   println!("{bass:?}");
+   
 }
