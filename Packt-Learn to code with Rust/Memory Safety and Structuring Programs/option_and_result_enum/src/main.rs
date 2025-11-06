d900e078a59cd60ffdd7b1d42b5cd66b1650a083
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 15:49:54 -0800

    returning option enum from a function

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/option_and_result_enum/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
   
-   let musical_instruments=[
+   /*let musical_instruments=[
     String::from("Guitar"),
     String::from("Drums"),
     String::from("bass"),
@@ -15,8 +15,25 @@ fn main() {
    }
    //let valid_instrument=bass.unwrap();
    //println!("{valid_instrument}");
-   let invalid_instrument=musical_instruments.get(3);
+   //let invalid_instrument=musical_instruments.get(3);
    //invalid_instrument.unwrap();
-   //invalid_instrument.expect("Index out of bounds");
+   //invalid_instrument.expect("Index out of bounds");*/
+   let availability=is_item_in_stock(true,false);
+   println!("{availability:?}");
+   match availability{
+      Option::Some(value)=>{println!("The item is available:{value}");}
+      Option::None=>{println!("The item doesn't exist in our system");}
+   }
    
+}
+fn is_item_in_stock(item_is_in_system:bool,item_is_in_stock:bool)->Option<bool>{
+  if item_is_in_system && item_is_in_stock{
+    Option::Some(true)
+  }
+  else if item_is_in_system{
+       Option::Some(false)
+  }
+  else{
+     Option::None
+  }
 }
