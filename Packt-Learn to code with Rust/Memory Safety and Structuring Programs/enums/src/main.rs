f0bbceab059dfa7867967150ec64c72559c7cac4
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-05 22:49:58 -0800

    match keyword-1

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -1,59 +1,19 @@
 #[derive(Debug)]
-enum CardSuit{
-    Hearts,
-    Diamonds,
-    Spades,
-    Clubs
+enum OperatingSystem{
+    Windows,
+    MacOS,
+    Linux
 }
-#[derive(Debug)]
-struct Credentials{
-    username:String,
-    password:String
+fn main(){
+    let my_os=OperatingSystem::MacOS;
+    let age_os= years_since_release(my_os);
+    println!("Age is {age_os}");
 }
-#[derive(Debug)]
-enum PaymentMethodType{
-    CreditCard(String),
-    DebitCard(String),
-    PayPal{username:String,password:String}
-}
-struct Card{
-    rank:String,
-    suit:CardSuit
-}
-#[derive(Debug)]
-enum Meat{
-    Chicken,
-    Steak
-}
-#[derive(Debug)]
-enum RestaurantItem{
-   Burrito(Meat),
-   Bowl(Meat),
-   VeganPlate
-}
-fn main() {
-    let first_card:CardSuit=CardSuit::Hearts;
-    let mut second_card:CardSuit=CardSuit::Spades;
-    second_card=CardSuit::Diamonds;
-    println!("{second_card:?}");
-    let card_suits=[CardSuit::Clubs,CardSuit::Spades];
-    let visa =PaymentMethodType::CreditCard(String::from("1234-5678"));
-    let master_card=PaymentMethodType::DebitCard(String::from("5678-9012"));
-    println!("{visa:?}");
-    println!("{master_card:?}");
-     let paypal_credentials=PaymentMethodType::PayPal {
-        username:String::from("abc@123.com"),
-        password:String::from("12345")
-     };
-    let mut my_payment_method= PaymentMethodType::CreditCard(String::from("123"));
-    my_payment_method=paypal_credentials;
-     println!("{my_payment_method:?}");
-     let lunch =RestaurantItem::Burrito(Meat::Steak);
-     let dinner =RestaurantItem::Bowl(Meat::Chicken);
-     let abandoned_meal= RestaurantItem::VeganPlate;
-     println!("Lunch was {lunch:?}");
-     println!("Dinner was {dinner:?}");
-     println!("Abandaoned meal was {abandoned_meal:?}");
-     
-    
+fn years_since_release(os:OperatingSystem)->u32{
+    match os {
+        OperatingSystem::Windows =>39 ,
+        OperatingSystem::MacOS =>23,
+        OperatingSystem::Linux => 34
+    }
+
 }
