50eaeafb2d8bfdd67a874f750364a5774f16be68
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-05 21:56:29 -0800

    Enums associated values 2

--- Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
+++ Packt-Learn to code with Rust/Memory Safety and Structuring Programs/enums/src/main.rs
@@ -9,7 +9,7 @@ enum CardSuit{
 enum PaymentMethodType{
     CreditCard(String),
     DebitCard(String),
-    PayPal(String)
+    PayPal(String, String)
 }
 struct Card{
     rank:String,
@@ -25,4 +25,7 @@ fn main() {
     let master_card=PaymentMethodType::DebitCard(String::from("5678-9012"));
     println!("{visa:?}");
     println!("{master_card:?}");
+    let mut my_payment_method= PaymentMethodType::CreditCard(String::from("123"));
+    my_payment_method=PaymentMethodType::PayPal(String::from("ani@123.com"),String::from("12345"));
+     println!("{my_payment_method:?}");
 }
