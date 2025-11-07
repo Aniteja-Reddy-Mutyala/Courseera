9645a5652a10d9318cce4d72e7ddaf62e8033344
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-06 18:23:01 -0800

    Adding and removing elements in vector

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/vectors/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/vectors/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-   let pizza_diameters:Vec<i32>=vec![8,10,12,14];
+   let mut pizza_diameters:Vec<i32>=vec![8,10,12,14];
    println!("{pizza_diameters:?}");
+   pizza_diameters.push(16);
+   pizza_diameters.push(18);
+   println!("{pizza_diameters:?}");
+   pizza_diameters.insert(0, 4);
+   println!("{pizza_diameters:?}");
+   pizza_diameters.pop();
+   println!("{pizza_diameters:?}");
+   pizza_diameters.remove(3);
+     println!("{pizza_diameters:?}");
    let pasta = vec!["hi","this","is","vector"];
    println!("{pasta:?}");
 }
