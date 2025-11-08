e9f3e534a5d56c83adc61f3570559c1ffdd16f88
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-07 19:07:58 -0800

    making talk_to_manager() func private

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/project_structure/src/inventory.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/project_structure/src/inventory.rs
@@ -1,6 +1,6 @@
 pub mod products;
 pub const FLOOR_SPACE: i32 = 10000;
 pub const MANAGER: &str = "Aniteja inventory";
-pub fn talk_to_manager() {
+fn talk_to_manager() {
     println!("Hi {MANAGER},how is your coffee");
 }
