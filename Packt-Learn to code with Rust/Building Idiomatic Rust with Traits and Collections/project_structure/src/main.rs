8acdbc974585c4a8911b86e77b93131a174ea881
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-07 18:10:49 -0800

    public struct,public fields and public enums

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/project_structure/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/project_structure/src/main.rs
@@ -4,4 +4,21 @@ mod orders;
 fn main() {
     println!("Hi {}", inventory::MANAGER);
     println!("My orders manager is {}", orders::MANAGER);
+    println!(
+        "Our managers are {} and {}.We have room space for {} sft.",
+        inventory::MANAGER,
+        orders::MANAGER,
+        inventory::FLOOR_SPACE
+    );
+    inventory::talk_to_manager();
+    let my_favourite_product = inventory::ProductCategory::Hammer;
+    println!("{my_favourite_product:?}");
+    let check = inventory::ProductCategory::Hammer;
+    println!("{check:?}");
+    let tall_ladder = inventory::Item {
+        name: String::from("Huge ladder"),
+        category: my_favourite_product,
+        quantity: 100,
+    };
+    println!("{tall_ladder:#?}");
 }
