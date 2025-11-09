1862114e57e3b5f53f0823a459c98b003ca53362
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-08 21:22:24 -0800

    Intro to Hashset

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/hashmaps/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/hashmaps/src/main.rs
@@ -1,7 +1,8 @@
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 fn main() {
-    let mut menu:HashMap<String, f64>=HashMap::new();
+    /*let mut menu:HashMap<String, f64>=HashMap::new();
     menu.insert("Steak".to_string(), 29.99);
     menu.insert("Tuna".to_string(), 29.99);
     menu.insert("Chicken".to_string(),25.99);
@@ -19,6 +20,16 @@ fn main() {
     println!("{}",france_capital.unwrap_or(&"Country doesn't exist in list"));
 
     country_capitals.entry("France").or_insert("paris");
-    println!("{country_capitals:?}");
+    println!("{country_capitals:?}");*/
+    let mut concert_queue= HashSet::<&str>::new();
+    concert_queue.insert("Molly");
+    concert_queue.insert("Megan");
+   println!("{concert_queue:?}");
+   println!("{}",concert_queue.len());
+   concert_queue.insert("Molly");
+   println!("{concert_queue:?}");
+   println!("{}",concert_queue.remove(&"Megan"));
+   println!("{}",concert_queue.contains(&"Megan"));
+   println!("{:?}",concert_queue.get(&"Megan").unwrap_or(&"Doesn't exist"));
 
 }
