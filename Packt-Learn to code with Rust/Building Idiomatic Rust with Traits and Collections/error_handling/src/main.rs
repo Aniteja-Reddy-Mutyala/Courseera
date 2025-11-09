3e8b7928407b773438442d27169cdd1a9640db25
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-08 23:03:57 -0800

    exit function of process submodule

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
@@ -1,4 +1,10 @@
+use std::process::exit;
+
 fn main() {
+    let array=[1,2];
     println!("Hello, world!");
-    panic!("Something went wrong");
+       exit(1);
+    println!("{:?}",array[2]);
+    //panic!("Something went wrong");
+ 
 }
