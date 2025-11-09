6d0e02f933504e51fa8d065647ea80f3c869efef
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-08 18:47:27 -0800

    format! macro

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
@@ -1,8 +1,12 @@
+
+
 fn main() {
   let  first_name:String=String::from("Aniteja Reddy");
   let  last_name= String::from("Mutyala");
-  let full_name = first_name + &last_name;
+  //let full_name = first_name + &last_name;
   //full_name.push_str(last_name);
-  println!("{full_name}");
+  let icon= format!("{first_name} {last_name}");
+  println!("{icon}");
+  println!("{first_name}");
   println!("{last_name}");
 }
