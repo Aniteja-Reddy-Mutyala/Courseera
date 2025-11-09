7c734f45cf50983926f40d027b6ecf8b595b3714
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-08 19:26:59 -0800

    string methods like trim,casing,split and replace

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
@@ -1,12 +1,18 @@
 
 
 fn main() {
-  let  first_name:String=String::from("Aniteja Reddy");
-  let  last_name= String::from("Mutyala");
+  //let  first_name:String=String::from("Aniteja Reddy");
+  //let  last_name= String::from("Mutyala");
   //let full_name = first_name + &last_name;
   //full_name.push_str(last_name);
-  let icon= format!("{first_name} {last_name}");
-  println!("{icon}");
-  println!("{first_name}");
-  println!("{last_name}");
+  //let icon= format!("{first_name} {last_name}");
+  //println!("{icon}");
+  //println!("{first_name}");
+  //println!("{last_name}");
+  let music_genre= "   rock ,metal,rap,pop";
+  println!("{}",music_genre.trim());
+  println!("{}",music_genre.trim().to_uppercase());
+  println!("{}",music_genre.replace("a", "@"));
+  let genres:Vec<&str>=music_genre.split(',').collect();
+  println!("{:?}",genres);
 }
