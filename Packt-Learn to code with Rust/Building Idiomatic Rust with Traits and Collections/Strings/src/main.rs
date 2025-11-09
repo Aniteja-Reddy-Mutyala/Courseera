6615045f953d16bcf7046324173f32b7cf910f07
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-08 19:45:44 -0800

    Collecting User Input with read_line Method

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/Strings/src/main.rs
@@ -1,5 +1,5 @@
 
-
+use std::io::stdin;
 fn main() {
   //let  first_name:String=String::from("Aniteja Reddy");
   //let  last_name= String::from("Mutyala");
@@ -15,4 +15,13 @@ fn main() {
   println!("{}",music_genre.replace("a", "@"));
   let genres:Vec<&str>=music_genre.split(',').collect();
   println!("{:?}",genres);
+  let mut name=String::new();
+   println!("What is your name?");
+  //stdin().read_line(& mut name);
+  match stdin().read_line(&mut name){
+    Result::Ok(_) =>{println!("Hi {}",name.trim());}
+    Result::Err(_)=>{println!("Error");}
+
+  }
+ 
 }
