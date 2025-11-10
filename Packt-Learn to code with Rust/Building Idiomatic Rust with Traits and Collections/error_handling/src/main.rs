0f24700e07d4a0ea4ba49e2300f92c882e4145f4
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-09 16:36:21 -0800

    eprintln! macro

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
@@ -3,8 +3,8 @@ use std::process::exit;
 fn main() {
     let array=[1,2];
     println!("Hello, world!");
-       exit(1);
-    println!("{:?}",array[2]);
+       //exit(1);
+   // println!("{:?}",array[2]);
     //panic!("Something went wrong");
- 
+    eprintln!("Something went wrong!!!")
 }
