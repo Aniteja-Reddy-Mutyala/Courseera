ccc41a1cc760c89fd816750b1ea8dfd0757144f8
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-09 21:24:58 -0800

    Asking user for input

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
@@ -1,16 +1,24 @@
 use std::fs::File;
+use std::io::stdin;
+use std::process;
 
 fn main() {
     let array=[1,2];
-    println!("Hello, world!");
+    println!("Please enter the name of file");
+    let mut input =String::new();
        //exit(1);
    // println!("{:?}",array[2]);
     //panic!("Something went wrong");
-    let file=File::open("story.txt");
+   
+   match stdin().read_line(&mut input){
+    Result::Ok(_)=>{}
+    Result::Err(_)=>{eprintln!("Something went wrong");process::exit(1);}
+   }
+    let file=File::open(input.trim());
     match file{
         Result::Ok(value)=>{println!("{value:?}");}
         Result::Err(_)=>{eprintln!("Error in opening file");}
     }
 
-    eprintln!("Something went wrong!!!")
+   // eprintln!("Something went wrong!!!")
 }
