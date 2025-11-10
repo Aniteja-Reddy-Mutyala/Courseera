2bb1fb4836e64d73d29dc594f9f7b67c5cbf5817
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-09 21:50:50 -0800

    Reading file content

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::stdin;
+use std::io::{stdin,Read};
 use std::process;
 
 fn main() {
@@ -14,10 +14,16 @@ fn main() {
     Result::Ok(_)=>{}
     Result::Err(_)=>{eprintln!("Something went wrong");process::exit(1);}
    }
-    let file=File::open(input.trim());
-    match file{
-        Result::Ok(value)=>{println!("{value:?}");}
-        Result::Err(_)=>{eprintln!("Error in opening file");}
+    let mut file:File=match File::open(input.trim()){
+  
+        Result::Ok(file)=>{file}
+        Result::Err(error)=>{eprintln!("Error in opening file.The error is {error}");process::exit(1);}
+    };
+    let mut file_content=String::new();
+    let read_operation=file.read_to_string(& mut file_content);
+    match &read_operation{
+        Result::Ok(_)=>{println!("{file_content}");}
+        Result::Err(error)=>{eprint!("Unable to read file due to {error}");process::exit(1);}
     }
 
    // eprintln!("Something went wrong!!!")
