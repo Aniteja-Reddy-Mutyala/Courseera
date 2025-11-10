f05dd3b5d8c48c17b51ccc5d84ed249075264a2d
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-09 23:10:23 -0800

    using read_to_string of fs module

--- Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
+++ Packt-Learn to code with Rust/Building Idiomatic Rust with Traits and Collections/error_handling/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+//use std::fs::File;
+use std::fs;
 use std::io::{Error, Read, stdin};
 //use std::process;
 
@@ -23,20 +24,21 @@ fn read_file() -> Result<String,Error> {
             return Err(error);
         }
     }*/
-    let mut file=File::open(input.trim())?;
+    fs::read_to_string(&input.trim())
+    //let mut file=File::open(input.trim())?;
     /*let mut file: File = match File::open(input.trim()) {
         Result::Ok(file) => file,
         Result::Err(error) => {
             return Result::Err(error);
         }
     };*/
-    let mut file_content = String::new();
-    file.read_to_string(&mut file_content)?;
+    //let mut file_content = String::new();
+    //file.read_to_string(&mut file_content)?;
     /*match read_operation {
         Result::Ok(_) => {}
         Result::Err(error) => {
             return Result::Err(error);
         }
     }*/
-    Result::Ok(file_content)
+    //Result::Ok(file_content)
 }
