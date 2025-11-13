093052757c53d0c65a4ede26fae81784f87f9ccb
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-13 12:39:43 -0800

    Embedding in attributes

--- Meta-Full stack developer/Basic React/creating_component/src/App.js
+++ Meta-Full stack developer/Basic React/creating_component/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
-
+function Logo(){
+  const userLogo=<img src={logo} alt={logo}/>
+  return userLogo;
+}
 function App() {
   return (
     <>
     <Header title="Welcome" color="purple"/>
+    <Logo/>
     </>
   );
 }
