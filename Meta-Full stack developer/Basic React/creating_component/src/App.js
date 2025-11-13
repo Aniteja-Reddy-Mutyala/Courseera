44808ced9a2e1bdc2fdf14dc487833051035433b
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-13 11:16:35 -0800

    Passing props

--- Meta-Full stack developer/Basic React/creating_component/src/App.js
+++ Meta-Full stack developer/Basic React/creating_component/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
-function Header(){
-  return <h1>Hello World!!!</h1>
-}
 function App() {
   return (
     <>
-    <Header/>
+    <Header title="Welcome"/>
     </>
   );
 }
