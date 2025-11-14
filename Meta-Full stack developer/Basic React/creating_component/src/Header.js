3df0fdfc25b3d00d5750009acaf615ac3a770be0
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-14 09:21:29 -0800

    adding a button

--- Meta-Full stack developer/Basic React/creating_component/src/Header.js
+++ Meta-Full stack developer/Basic React/creating_component/src/Header.js
@@ -1,5 +1,10 @@
+const handleClick=()=><p style={{borderColor:"blue"}}>You have clicked me</p>
 function Header({title,color}){
-  return <h1 style={{color:color}}>{title}</h1>
-}
+  return (
+  <>
+  <h1 style={{color:color}}>{title}</h1>
+  <button onClick={handleClick}> Click me please</button>
+  </>
+)}
 
 export default Header
