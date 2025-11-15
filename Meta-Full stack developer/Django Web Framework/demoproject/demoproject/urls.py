daaf953dc1811524b508621d865504f226cb8bf1
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-15 10:45:15 -0800

    custom error handling

--- Meta-Full stack developer/Django Web Framework/demoproject/demoproject/urls.py
+++ Meta-Full stack developer/Django Web Framework/demoproject/demoproject/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
 
     
 ]
+handler404="demoproject.views.handler404"
---
+++ Meta-Full stack developer/Django Web Framework/demoproject/demoproject/views.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def handler404(request,exception):
+    return HttpResponse("404: Page not found")
