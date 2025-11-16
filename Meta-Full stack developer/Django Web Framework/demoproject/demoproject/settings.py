061384c0b5d7e3ebed82e076442e733d1ba17d37
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-15 17:46:10 -0800

    Created a django user

--- Meta-Full stack developer/Django Web Framework/demoproject/demoproject/settings.py
+++ Meta-Full stack developer/Django Web Framework/demoproject/demoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g*cca&_wu!n3ee_fu=k-l@(jbq)smp+flay_*(=3-q&7_y7@_a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
