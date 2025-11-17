b1d657b645677237ef2d5666b49685bfc08bac0e
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-11-16 22:22:38 -0800

    class based views

--- Meta-Full stack developer/Api/BookList/BookList/BookListApi/urls.py
+++ Meta-Full stack developer/Api/BookList/BookList/BookListApi/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("books/",views.books,name="books")
+    path("books/",views.BookList.as_view(),name="books")
     
 ]
--- Meta-Full stack developer/Api/BookList/BookList/BookListApi/views.py
+++ Meta-Full stack developer/Api/BookList/BookList/BookListApi/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+from rest_framework.views import APIView
 # Create your views here.
 @api_view(['GET','POST'])
 def books(request):
     return Response('list of books',status=status.HTTP_200_OK)
+
+class BookList(APIView):
+    def get(self,request):
+        return Response('list of books',status=status.HTTP_200_OK)
+    def post(self,request):
+        return Response('new book created',status=status.HTTP_201_CREATED)
